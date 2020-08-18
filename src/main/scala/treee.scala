6c54708d6870a9e3779504081877886abce163f4
kpestolova <pestolova01@gmail.com>
2020-08-18 17:55:05 +0300

    Tree Homework

---
+++ src/main/scala/treee.scala
@@ -0,0 +1,25 @@
+import scala.io.StdIn.readLine
+
+object treee {
+
+  def main (args: Array [String]): Unit = {
+    var tree = 0
+    println("Make a tree by using odd numbers from 0 to 6!")
+    tree = readLine.toInt
+    if (tree == 1) println("  *  ")
+    else if (tree == 3) println(" *** ")
+    else if (tree == 5) println("*****")
+    else println("It's not an odd number!")
+
+    tree = readLine.toInt
+    if (tree == 1) println("  *  ")
+    else if (tree == 3) println(" *** ")
+    else if (tree == 5) println("*****")
+    else println("It's not an odd number!")
+
+    tree = readLine.toInt
+    if (tree == 1) println("  *  ")
+    else if (tree == 3) println(" *** ")
+    else if (tree == 5) println("*****")
+    else println("It's not an odd number!") } ; }
+
